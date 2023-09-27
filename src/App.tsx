abacfcc82b90d2f03c8a66a6c99f165fc14b7718
Lucía <luciamunozdev@gmail.com>
2023-09-27 16:27:10 +0200

    comments added to the code

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,10 @@ declare global {
   }
 }
 
+// The testnet api url used to connect to the Hedera testnet
 const testnetApiUrl = "https://testnet.hashio.io/api";
 
+// The Hedera testnet chain configuration
 const hederaTestnet = {
   id: 296,
   name: "Hedera Testnet",
@@ -37,67 +39,90 @@ const hederaTestnet = {
   },
 } as const satisfies Chain;
 
+// The chains used in the application
 const chains = [hederaTestnet];
+
+// The projectId for the WalletConnect integration
 const projectId = "bc8a48da849089da783de2987986630f";
 
+// The public client used in the application
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 
+// The wagmi configuration used in the application
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, chains }),
   publicClient,
 });
 
+// The ethereum client used in the application
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
+// Variable used to store the token id
 let tokenId;
 
+// Variables used to store the provider and signer
 let provider: ethers.BrowserProvider;
 let signer: ethers.Signer;
 
+// Function used to initialize the provider and signer variables
 async function initProvider() {
   provider = new ethers.BrowserProvider(window.ethereum);
   signer = await provider.getSigner();
 }
 
+// Function used to create a fungible token using the SDK
 async function createToken() {
   console.log("creating token...")
   tokenId = await createFungibleToken("Mintable Token", "MT");
   alert("Token created successfully with id: " + tokenId);
 }
 
+// Function used to transfer a fungible token using the smart contract
 async function transferMintableToken() {
+
   if (!provider) initProvider();
+
+  // Request account access if needed
   const [account] = await window.ethereum.request({
     method: "eth_requestAccounts",
   });
 
+  // Get the signer
   signer = await provider.getSigner();
 
   try {
+    // Get the solidity address from the token id
     const mintableTokenContractAddress = '0x' + tokenId!.toSolidityAddress();
 
+    // Create the contract instance to interact with the smart contract
     const mintableTokenContract = new ethers.Contract(
       mintableTokenContractAddress,
       mintableTokenAbi,
       signer,
     );
 
+    // Get the balance of the account
     let balance = await mintableTokenContract.balanceOf(account);
     console.log("Balance of the metamask account: ", balance.toString());
 
+    // Create a new account and associate it to the token so that it can receive tokens
     const { accountId, privateKey } = await createAccount();
 
+    // Associate function from the SDK
     await tokenAssociate(accountId!, privateKey, tokenId!);
 
+    // Get the solidity address from the new account id
     const newAccountAddress = '0x' + accountId!.toSolidityAddress();
 
-    const transfer = await mintableTokenContract.transfer(
-      newAccountAddress, "2", { gasLimit: 1000000 });
+    // Transfer 2 tokens to the new account
+    const transfer = await mintableTokenContract.transfer( newAccountAddress, "2", { gasLimit: 1000000 });
     const txTransfer = await transfer.wait();
     const transferStatus = txTransfer.status === 1 ? "SUCCESS" : "FAIL";
     alert("Transfer transaction finish with status: " + transferStatus);
     console.log("The transfer transaction finish with status: ", txTransfer.status.toString());
+
+    // Check the balance of the account after the transfer
     balance = await mintableTokenContract.balanceOf(account);
     console.log("The balance of the metamask account is: ", balance.toString());
   }
